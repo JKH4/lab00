fa94716e6ae322d3d6a1fef02294c03e58d32f73
Jalil KHAYI <jkhayi@d2si.io>
2019-01-18 16:38:10 +0100

    lot of stuff

---
+++ webapp/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "training-tf"
+    key    = "WebApp/terraform.tfstate"
+    region = "eu-west-1"
+    # dynamodb_table = "training-tf-webapp"
+  }
+}
--- webapp/webapp.tf
+++ webapp/webapp.tf
@@ -13,9 +13,59 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "template_file" "userData" {
+  template = "${file("userdata.tpl")}"
+  vars = {
+    username = "Jalil 4 da win"
+  }
+}
+
+data "aws_vpc" "targetVPC" {
+  filter = {
+    name = "tag:ENV"
+    values = ["training-tf"]
+  }
+}
+
+data "aws_subnet_ids" "targetSubnetIds" {
+  # availability_zone = "${var.region}*"
+  vpc_id = "${data.aws_vpc.targetVPC.id}"
+  filter = {
+    name = "tag:ENV"
+    values = ["training-tf"]
+  }
+}
+
+resource "aws_security_group" "mySecurityGroup" {
+  name        = "allow web and ssh"
+  description = "Allow web and ssh"
+  vpc_id      = "${data.aws_vpc.targetVPC.id}"
+  tags = {
+    Name = "mySecurityGroup"
+    ENV  = "training-tf"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+}
+
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+  user_data     = "${data.template_file.userData.rendered}"
+  subnet_id = "${element(data.aws_subnet_ids.targetSubnetIds.ids, 0)}"
+  vpc_security_group_ids = ["${aws_security_group.mySecurityGroup.id}"]
+  associate_public_ip_address = true
 
   tags = {
     Name = "myVM"
